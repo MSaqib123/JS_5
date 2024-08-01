8eb4d6bcf41649cd05cd332625329d7fb30463b0
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-08-01 23:02:48 +0500

    Adding default paramter

--- 1. Default Parameters.js
+++ 1. Default Parameters.js
@@ -1,31 +1,20 @@
+//-------------- Defalt Paramter -----------------
 
-// Data needed for first part of the section
-const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-    order : function(startIndex,menuIndex){
-      return [this.starterMenu[startIndex] , this.mainMenu[menuIndex]]
-    },
-  
-    openingHours: {
-      thu: {
-        open: 12,
-        close: 22,
-      },
-      fri: {
-        open: 11,
-        close: 23,
-      },
-      sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-      },
-    },
-  };
-  
+let Bookings = []
 
-  
-  
+//1. setting default value to paramters
+//2. we Can also  multiply --> apply any opertor to  Previews  Paramter  Example
+const createBooking = (flightNum,numPassengers=1, price=numPassengers*90) => {
+  const booking = {
+    flightNum,//: flightNum,
+    numPassengers,
+    price
+  }
+  console.log(booking)
+  Bookings.push(booking)
+}
+
+//if i will not give parmater values it will add undifned value to booking
+createBooking('C253')
+createBooking('C254',2)
+createBooking('C254',4,300)
