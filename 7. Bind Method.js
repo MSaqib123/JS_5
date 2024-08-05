2ba9ba503248d96691b11b249a029374c5523137
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-08-05 09:12:11 +0500

    adding some working

--- 7. Bind Method.js
+++ 7. Bind Method.js
@@ -1,5 +1,6 @@
 //_______________ Bind Methods ______________
 //Bind method is also like  Call 
+//its not emiduatly call the  method insetead return new method
 //but its return new function  instead of giving direct values
 
 
@@ -18,3 +19,28 @@ const PA = {
       })
     }
   }
+
+const PA_SINDH = {
+    airline : 'PA_Sindh',
+    iataCode : 'PA_SI',
+    bookings : [],
+}
+const swish = {
+    airline : 'swish',
+    iataCode : 'sw',
+    bookings : [],
+}
+const book = PA.book;  //now book function store in book
+
+
+//---- Bind method ------
+//let mujha baar  barr  (this -> object)  ko point out ni krnaa
+//then i will use bind method
+const bookPASI = book.bind(PA_SINDH)
+console.log(bookPASI) //its return new method
+bookPASI(44,"SAQIB sahad")
+
+
+//___ Example 1 ____
+//let mujhaa   booking ka waqt  ---> Flight same rkhni ha   har bar booking ka duran
+//instead of adding fligh again again we use  Bind method
